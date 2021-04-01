207a1d128534e09aeacb0e0b0c6d8ef63fbb1526
IidaTakuma <ta9maxxx@gmail.com>
2021-04-01 14:42:16 +0900

    chap02 fin.

--- main/settings.py
+++ main/settings.py
@@ -69,8 +69,17 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'main.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 ASGI_APPLICATION = 'main.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)]
+        }
+    }
+}
 
 
 # Database
